45c4e4e4a6a354db9f0e571499df1c0354f3f189
Matthew Lopez <lopezmatt6891@gmail.com>
2020-05-14 11:45:29 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+//  Setup empty JS object to act as endpoint for all routes
+projectData = [];
+
+//  Require Express to run server and routes
+const express = require('express');
+//  Start up an instance of app
+const app = express();
+/* Middleware*/
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
+
+//  Initialize the main project folder
+app.use(express.static('website'));
+
+//  Setup Server
+const port = process.env.PORT || 3000;
+
+//  Callback console.log to debug
+const server = app.listen(port, () => {
+  return console.log(`Express server listening on port ${port}...`);
+});
+
+//  GET route
+app.get('/all', sendData );
+
+//  Callback function to complete GET '/all'
+function sendData(request, response, next) {
+  response.send(projectData);
+};
+
+//  POST route
+app.post('/add', addPost );
+
+//  Callback function to complete POST '/add'
+function addPost (req, res, next) {
+  let newData = JSON.stringify(req.body);
+  projectData.push(newData);
+  //  Debug code console test
+  console.log(newData);
+  return res.send(newData);
+};
---
+++ website/app.js
@@ -0,0 +1,105 @@
+/* Global Variables */
+const ApiKey = "91b0b55d837ff53bcd4a0c367014bd60";
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+
+/* Helper Functions */
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'/'+ d.getDate()+'/'+ d.getFullYear();
+
+/* Event Listener(s) */
+document.getElementById("generate").addEventListener("click", performAction);
+
+// Kick off app functions using promises
+function performAction(e) {
+  let zip = document.getElementById("zip").value;
+
+  getWeatherData(`${baseURL}${zip}&units=imperial&appid=${ApiKey}`)
+    .then(function getTemp(newData) {
+      let temperature = newData.main.temp;
+      //  Debug test
+      console.log(`temp is ${temperature} Fahrenheit`);
+      return temperature;
+    })
+    .then(function createEntry(temperature) {
+      let feelings = document.getElementById("feelings").value;
+      //  Debug test
+      console.log(feelings);
+      let object = {
+        date: newDate,
+        temp: `${temperature} degrees Fahrenheit`,
+        content: feelings
+      };
+      //  Debug test
+      console.log(JSON.stringify(object));
+      return object;
+    })
+    .then((object) => {
+      postWeatherData("http://localhost:3000/add", object)
+    })  
+    .then(() => {
+      return getNewData("http://localhost:3000/all");      
+    })
+    .then((projectData) => {
+      uiUpdate(projectData);
+    })
+    .catch((error) => {
+      console.log("Total fail error", error);
+    });
+}
+
+//  Function to retrieve API weather data
+const getWeatherData = async (url) => {
+  const response = await fetch(url);
+  try {
+    let newData = await response.json();
+    console.log(`Successful Retrieval: ${JSON.stringify(newData)}`);
+    return newData;
+  } catch (error) {
+    console.log("Retrieval Error:", error);
+  }
+};
+
+//  Function to post weather
+const postWeatherData = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+  try {
+    const postData = await response.json();
+    console.log(`Successful Post: ${JSON.stringify(postData)}`);
+  } catch (error) {
+    console.error("Post Error:", error);
+  }
+};
+
+//  Fetch the data from the app endpoint
+const getNewData = async (url = "") => {
+  const response = await fetch(url);
+  try {
+    const projectData = await response.json();
+    console.log(`Successful Retrieval: ${projectData}`);
+    return projectData;
+  } catch (error) {
+    console.log("Retrieval Error:", error);
+  }
+};
+
+//  Update UI with fetched data from projectData 
+const uiUpdate = function(data) {
+  
+  let newArray = JSON.parse(data);
+  let date = newArray.date;
+  let temp = newArray.temp;
+  let content = newArray.content;
+  //  Debug check  
+  console.log(`newArray is a ${typeof newArray}`);
+  
+  document.getElementById("date").innerHTML = `The date is ${date}.`;
+  document.getElementById("temp").innerHTML = `The current temperature is ${temp}.`;
+  document.getElementById("content").innerHTML = `You're feeling ${content}.`;
+};
