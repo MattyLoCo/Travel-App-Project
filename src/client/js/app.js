0944188b226ffda467d59fe238e79abd4706d0e9
Matthew Lopez <lopezmatt6891@gmail.com>
2020-05-26 11:44:13 -0600

    chore: Update app js to include weatherapi fetch

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,16 +1,12 @@
 import { getCityData } from './getcitydata.js';
 import { dateCountdown } from './countdown';
+import { weatherBitAPI } from './weatherbitapi';
 
 // Kick off app functions using promises
 export function performAction(e) {
   getCityData()
   .then( dateCountdown() )
-    // .then(() => {
-    //   return getNewData("http://localhost:3000/all");      
-    // })
-    // .then((projectData) => {
-    //   uiUpdate(projectData);
-    // })
+  .then( weatherBitAPI() )
   .catch((error) => {
     console.log("Total fail error", error);
   });
